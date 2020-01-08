b55134f0b9ca6ee73990d40326b343f9fb76e04b
yubeihuang <hybbeck@gmail.com>
2020-01-08 16:21:57 +0800

    add ch02_03.cpp

---
+++ Chapter02/ch02_03.cpp
@@ -0,0 +1,20 @@
+// Author: Yubei Huang
+// Date: 2020-01-08
+
+#include <iostream>
+
+int main() {
+    std::cout << "[unsigned int] u = 10, u2 = 42." << std::endl;
+    unsigned u = 10, u2 = 42;
+    std::cout << "u2 - u = " << u2 - u << std::endl;
+    std::cout << "u - u2 = " << u - u2 << std::endl;
+
+    std::cout << "[int] i = 10, i2 = 42" << std::endl;
+    int i = 10, i2 = 42;
+    std::cout << "i2 - i = " << i2 - i << std::endl;
+    std::cout << "i - i2 = " << i - i2 << std::endl;
+    std::cout << "i - u = " << i - u << std::endl;
+    std::cout << "u - i = " << u - i << std::endl;
+
+    return 0;
+}
